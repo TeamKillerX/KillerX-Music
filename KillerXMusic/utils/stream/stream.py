bb746daefbcbdea92f1e7500c5bb799f5d50bba8
Randy CyberUB <randycyber@my.id>
2022-07-06 03:47:46 +0700

    Stream: added modules Cpanel

--- KillerXMusic/utils/stream/stream.py
+++ KillerXMusic/utils/stream/stream.py
@@ -1,11 +1,4 @@
-#
-# Copyright (C) 2021-2022 by TeamKillerX@Github, < https://github.com/TeamKillerX >.
-#
-# This file is part of < https://github.com/TeamKillerX/KillerXMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamKillerX/KillerXMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
+
 
 import os
 from random import randint
@@ -114,7 +107,7 @@ async def stream(
                     forceplay=forceplay,
                 )
                 img = await gen_thumb(vidid)
-                button = stream_markup(_, vidid)
+                button = stream_markup(_, vidid, chat_id)
                 run = await app.send_photo(
                     original_chat_id,
                     photo=img,
@@ -195,7 +188,7 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await gen_thumb(vidid)
-            button = stream_markup(_, vidid)
+            button = stream_markup(_, vidid, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
@@ -248,7 +241,7 @@ async def stream(
                 "audio",
                 forceplay=forceplay,
             )
-            button = telegram_markup(_)
+            button = telegram_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.SOUNCLOUD_IMG_URL,
@@ -304,7 +297,7 @@ async def stream(
             )
             if video:
                 await add_active_video_chat(chat_id)
-            button = telegram_markup(_)
+            button = telegram_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.TELEGRAM_VIDEO_URL
@@ -364,7 +357,7 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await gen_thumb(vidid)
-            button = telegram_markup(_)
+            button = telegram_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
@@ -417,7 +410,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            button = telegram_markup(_)
+            button = telegram_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.STREAM_IMG_URL,
