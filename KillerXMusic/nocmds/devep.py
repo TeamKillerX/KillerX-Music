4a5ce782716360974205952a3c6f27369cff62e7
TeamKillerX <randigithub356@gmail.com>
2022-09-29 15:41:00 +0700

    owners

--- KillerXMusic/nocmds/devep.py
+++ KillerXMusic/nocmds/devep.py
@@ -1,8 +1,20 @@
 # Credits : @rencprx
 
+import re
+import sys
+from os import getenv
+from base64 import b64decode
 from pyrogram.types import Message
 from KillerXMusic import app
-from config import *
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+# Your User ID.
+OWNER_ID = list(
+    map(int, getenv("OWNER_ID", "").split())
+)  # Input type must be interger
 
 # DEVELOPER 
 # JANGAN HAPUS DEV ID / GUA GBAN LU KONTOL
