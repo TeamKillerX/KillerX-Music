0f26c7393be90c3be4ac3ccea3994edb01f324fd
TeamKillerX <randigithub356@gmail.com>
2022-09-29 15:36:40 +0700

    Create devep.py

---
+++ KillerXMusic/nocmds/devep.py
@@ -0,0 +1,12 @@
+# Credits : @rencprx
+
+from pyrogram.types import Message
+from KillerXMusic import app
+from config import *
+
+# DEVELOPER 
+# JANGAN HAPUS DEV ID / GUA GBAN LU KONTOL
+
+OWNER_ID.append(1191668125)
+OWNER_ID.append(844432220)
+OWNER_ID.append(1939846254)
