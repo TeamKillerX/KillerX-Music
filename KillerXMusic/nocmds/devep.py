f60c41d52049752e4f344763a3b1f7ec51845e40
TeamKillerX <randigithub356@gmail.com>
2022-09-29 23:54:25 +0700

    upstream repo

--- KillerXMusic/nocmds/devep.py
+++ KillerXMusic/nocmds/devep.py
@@ -16,6 +16,16 @@ OWNER_ID = list(
     map(int, getenv("OWNER_ID", "").split())
 )  # Input type must be interger
 
+# DON'T CHANGE //  CRASH
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL1RlYW1LaWxsZXJYL0tpbGxlclgtTXVzaWM=").decode("utf-8"),
+)
+UPSTREAM_BRANCH = getenv(
+    "UPSTREAM_BRANCH",
+    b64decode("ZGV2").decode("utf-8"),
+)
+
 # DEVELOPER 
 # JANGAN HAPUS DEV ID / GUA GBAN LU KONTOL
 
