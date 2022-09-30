3c01fcd09d757a76c888ba0b90a223d51bdaeb96
TeamKillerX <randigithub356@gmail.com>
2022-09-30 16:36:55 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -26,7 +26,7 @@ API_HASH = getenv("API_HASH")
 ## Get it from @Botfather in Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-# WORKERS = int(getenv("WORKERS" 4))
+workers = int(getenv("WORKERS"))
 
 # BOTFATHER WITHOUT @
 BOT_USERNAME = getenv("BOT_USERNAME")
