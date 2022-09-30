0cf6d19a0142b18d3fb995e57d61335e56ced024
TeamKillerX <randigithub356@gmail.com>
2022-09-30 21:11:36 +0700

    Update assistant_on.py

--- KillerXMusic/plugins/assistant/assistant_on.py
+++ KillerXMusic/plugins/assistant/assistant_on.py
@@ -9,4 +9,5 @@ from KillerXMusic import app
 from KillerXMusic.nocmds.prefix import *
 
 @app.on_message(command("asst_on") & other_filters) 
-     await ass.one.send_message(message.chat.id, "Assistant Online") 
+async def asst():
+    await ass.one.send_message(message.chat.id, "Assistant Online") 
