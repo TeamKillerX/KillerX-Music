c7524d64274581ce48f835885d3fa72a6778d716
TeamKillerX <randigithub356@gmail.com>
2022-12-04 17:31:04 +0700

    ok

--- KillerXMusic/nocmds/devep.py
+++ KillerXMusic/nocmds/devep.py
@@ -17,8 +17,9 @@ OWNER_ID = list(
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "randydev.my.id",
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL1RlYW1LaWxsZXJYL0tpbGxlclgtTXVzaWM=").decode("utf-8"),
 )
+
 UPSTREAM_BRANCH = getenv(
     "UPSTREAM_BRANCH",
     b64decode("ZGV2").decode("utf-8"),
