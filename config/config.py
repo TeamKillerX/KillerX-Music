2ff952c482d3e4a2282333bfcf2593e2267ab9d0
TeamKillerX <randigithub356@gmail.com>
2022-12-05 21:25:47 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -20,7 +20,7 @@ from KillerXMusic.nocmds.devep import *
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = int(getenv("API_ID"))
+API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
 
 ## Get it from @Botfather in Telegram.
