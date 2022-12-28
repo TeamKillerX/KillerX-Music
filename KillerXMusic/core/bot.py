e7ead198757edad8a31bb1a7a85ec230cc795a62
xtsea <90479255+xtsea@users.noreply.github.com>
2022-12-29 01:14:05 +0700

    _

--- KillerXMusic/core/bot.py
+++ KillerXMusic/core/bot.py
@@ -46,10 +46,11 @@ class KillerXBot(Client):
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status == ChatMemberStatus.ADMINISTRATOR:
             status = "Administrator"
-#            LOGGER(__name__).error("Please promote Bot as Admin in Logger Group"
+            LOGGER(__name__).error("Please promote Bot as Admin in Logger Group"
 
-#            )
+            )
             sys.exit()
+
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
